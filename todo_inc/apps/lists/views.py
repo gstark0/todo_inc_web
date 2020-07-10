35a05cdca53224b10b6d5d1644256c188e51d538
gstark0 <gstark0@icloud.com>
2020-07-10 14:16:55 +0200

    Basic lists html template

--- todo_inc/apps/lists/views.py
+++ todo_inc/apps/lists/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def show_lists(request):
-	test = '<h1>This is LISTS app</h1>'
-	return HttpResponse(test)
+	return render(template_name='lists.html', request=request)
