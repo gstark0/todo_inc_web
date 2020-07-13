0277e33bd5a67fffc25e8c9b40e0f059fa50c510
gstark0 <gstark0@icloud.com>
2020-07-13 14:05:56 +0200

    Render lists

--- todo_inc/apps/lists/views.py
+++ todo_inc/apps/lists/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import List, Task
 
 def show_lists(request):
-	return render(template_name='lists.html', request=request)
+	lists = List.objects.all()
+	return render(template_name='lists.html', request=request, context={'lists': lists})
