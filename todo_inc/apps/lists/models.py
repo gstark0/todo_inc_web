ec80a1acfd584f5ecad8096850cca576205949c9
gstark0 <gstark0@icloud.com>
2020-07-13 13:27:14 +0200

    Add foreign key to Task model

--- todo_inc/apps/lists/models.py
+++ todo_inc/apps/lists/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 
-class list(models.Model):
+class List(models.Model):
 	name = models.CharField(max_length=50)
 	code = models.CharField(max_length=4)
 
-class task(models.Model):
+class Task(models.Model):
 	name = models.CharField(max_length=50)
 	completed = models.BooleanField(default=False)
+	list_id = models.ForeignKey(List, on_delete=models.CASCADE)
