baf2329aaa110ac2b92904b6b7b5cf62ca87565f
gstark0 <gstark0@icloud.com>
2020-07-13 15:01:16 +0200

    Render tasks list

--- todo_inc/apps/lists/urls.py
+++ todo_inc/apps/lists/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.show_lists)
+	path('', views.show_lists),
+	path('<str:code>/', views.render_list)
 ]
--- todo_inc/apps/lists/views.py
+++ todo_inc/apps/lists/views.py
@@ -5,3 +5,8 @@ from .models import List, Task
 def show_lists(request):
 	lists = List.objects.all()
 	return render(template_name='lists.html', request=request, context={'lists': lists})
+
+def render_list(request, code):
+	tasks = Task.objects.filter(list_id__code=code)
+	list_item = List.objects.get(code=code)
+	return render(template_name='list.html', request=request, context={'tasks': tasks, 'list': list_item})
