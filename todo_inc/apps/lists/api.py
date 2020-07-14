38f95d1ab0b05194c9cbc5db914eec80269c7a42
gstark0 <gstark0@icloud.com>
2020-07-14 19:54:44 +0200

    Verify if codes exist

--- todo_inc/apps/lists/api.py
+++ todo_inc/apps/lists/api.py
@@ -8,6 +8,9 @@ from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.views.decorators.http import require_http_methods
 from .models import List, Task
 
+positive_response = {'response': 1}
+negative_response = {'response': 0}
+
 @csrf_exempt
 @require_http_methods(['GET', 'DELETE'])
 def specific_list(request, code=None):
@@ -20,17 +23,26 @@ def specific_list(request, code=None):
 		try:
 			list_instance = List.objects.get(code=code)
 			list_instance.delete()
-			return JsonResponse({'response': 1})
+			return JsonResponse(positive_response)
 		except List.DoesNotExist:
-			return JsonResponse({'response': 0})
+			return JsonResponse(negative_response)
 
 @csrf_exempt
 @require_http_methods(['GET', 'POST'])
 def general_lists(request):
 	# GET all lists from DB
 	if request.method == 'GET':
-		all_lists = List.objects.all().values('code', 'name')
-		return JsonResponse({'lists': list(all_lists)})
+		# Try to get codes from url to confirm if codes exist in DB
+		codes = request.GET.get('codes', '')
+		if codes is not '':
+			codes = codes.split(',')
+			lists = List.objects.filter(code__in=codes).values('name', 'code')
+
+			return JsonResponse({'lists': list(lists)})
+		# If codes param doesn't exist, just display all lists from DB
+		else:
+			all_lists = List.objects.all().values('code', 'name')
+			return JsonResponse({'lists': list(all_lists)})
 
 	# Generate a new list and return its code
 	elif request.method == 'POST':
@@ -47,20 +59,23 @@ def general_lists(request):
 @csrf_exempt
 @require_http_methods(['DELETE'])
 def specific_task(request, task_id=None):
+	# DELETE a specific task
 	if request.method == 'DELETE':
 		try:
 			task_instance = Task.objects.get(pk=task_id)
 			task_instance.delete()
-			return JsonResponse({'response': 1})
+			return JsonResponse(positive_response)
 		except Task.DoesNotExist:
-			return JsonResponse({'response': 0})
+			return JsonResponse(negative_response)
 
 @csrf_exempt
 @require_http_methods(['GET', 'POST'])
 def general_tasks(request):
+	# GET all tasks
 	if request.method == 'GET':
 		all_tasks = Task.objects.all().values('pk', 'name', 'completed', 'list_id__code')
 		return JsonResponse({'tasks': list(all_tasks)})
+	# Add a new task
 	elif request.method == 'POST':
 		data = json.loads(request.body)
 		task_name = data['name']
@@ -69,18 +84,19 @@ def general_tasks(request):
 			list_instance = List.objects.get(code=list_code)
 			new_task = Task(name=task_name, list_id=list_instance)
 			new_task.save()
-			return JsonResponse({'response': 1})
+			return JsonResponse(positive_response)
 
 		except List.DoesNotExist:
-			return JsonResponse({'response': 0})
+			return JsonResponse(negative_response)
 
 @require_http_methods(['GET'])
 def complete_task(request, task_id=None):
+	# Mark a task as "completed" (or "not completed" if it was completed before)
 	try:
 		task_instance = Task.objects.get(pk=task_id)
 		task_instance.completed = not task_instance.completed
 		task_instance.save()
-		return JsonResponse({'response': 1})
+		return JsonResponse(positive_response)
 
 	except Task.DoesNotExist:
-		return JsonResponse({'response': 0})
+		return JsonResponse(negative_response)
