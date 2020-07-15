675749cc18e137d139acb66b4aa97787c483efc6
gstark0 <gstark0@icloud.com>
2020-07-15 18:26:22 +0200

    Add new tasks to DB through the UI

--- todo_inc/apps/lists/urls.py
+++ todo_inc/apps/lists/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
 	path('', views.show_lists),
 	path('<str:code>/', views.render_list),
 
-	path('api/lists/<str:code>', api.specific_list),
-	path('api/lists', api.general_lists),
+	path('api/lists/<str:code>/', api.specific_list),
+	path('api/lists/', api.general_lists),
 
-	path('api/tasks/<int:task_id>', api.specific_task),
-	path('api/tasks/<int:task_id>/complete', api.complete_task),
-	path('api/tasks', api.general_tasks)
+	path('api/tasks/<int:task_id>/', api.specific_task),
+	path('api/tasks/<int:task_id>/complete/', api.complete_task),
+	path('api/tasks/', api.general_tasks)
 ]
