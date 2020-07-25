a25d9a9fa6a59a21f1b04788db4023e8e565f22e
gstark0 <gstark0@icloud.com>
2020-07-25 13:02:43 +0200

    Generate task's primary key randomly

--- todo_inc/apps/lists/models.py
+++ todo_inc/apps/lists/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.utils import timezone
+import random
 
+def generate_pk():
+	number = random.randint(1000, 200000)
+	return number
 
 class List(models.Model):
 	name = models.CharField(max_length=50)
 	code = models.CharField(max_length=4)
 
 class Task(models.Model):
+	id = models.CharField(default=generate_pk, primary_key=True, max_length=255, unique=True)
 	name = models.CharField(max_length=50)
 	completed = models.BooleanField(default=False)
 	list_id = models.ForeignKey(List, on_delete=models.CASCADE)
